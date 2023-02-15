f83af8ab40ddccfd75b1c9be7a1a5d7b78cec49b
Sukhmeet_Singh <sukhmeet_singh@persistent.com>
2023-02-15 11:06:24 +0530

    register_UI

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -39,12 +39,12 @@ const CartItems = (props) => {
   const { title, image, price ,qty } = props.item;
 
   return (
-    <>
+    /*<>
       <div className="col-12 p-2">
         <div className="cartProduct container d-flex">
           <div className="card col-3 text-red bg-navyBlue">
             <div className="card-body">
-              {/* <img src={image}/> */}
+              {// <img src={image}/> }
               <div className="cartProduct1" height="250px">
                 <img
                   src={image}
@@ -90,8 +90,48 @@ const CartItems = (props) => {
         </div>
       </div>
     </>
+  );*/
+  <div class="body1">
+  <div class="CartContainer1">
+  <div class="Header1">
+    <h3 class="Heading1">Shopping Cart</h3>
+    <h5 class="Action1">Remove all</h5>
+  </div>
+
+  <div class="Cart-Items1">
+      <div class="image-box1">
+        <img src="images/apple.png" style={{  }} />
+      </div>
+      <div class="about1">
+        <h1 class="title1">Apple Juice</h1>
+        <h3 class="subtitle1">250ml</h3>
+        <img src="images/veg.png" style={{  }}/>
+      </div>
+      <div class="counter1">
+        <div class="btn1">+</div>
+        <div class="count1">2</div>
+        <div class="btn1">-</div>
+      </div>
+      <div class="prices1">
+        <div class="amount1">$2.99</div>
+        <div class="save1"><u>Save for later</u></div>
+        <div class="remove1"><u>Remove</u></div>
+      </div>
+  </div>
+  
+<div class="checkout1">
+<div class="total1">
+  <div>
+    <div class="Subtotal1">Sub-Total</div>
+    <div class="items1">2 items</div>
+  </div>
+  <div class="total-amount1">$6.18</div>
+</div>
+<button class="button">Checkout</button></div>
+</div>
+</div>
   );
-};
+}
 
 
 export default Cart;
